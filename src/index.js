2a6d6aa994803f52915a4358fa0e2d7d569beb71
Marcos Almeida <marcoshenriqueb@gmail.com>
2018-09-08 12:05:23 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'grommet/scss/vanilla/index.scss';
+import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root')); // eslint-disable-line
+// registerServiceWorker();
